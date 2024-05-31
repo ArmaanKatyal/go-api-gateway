e4b9e958f3cb52400f70bcc5bf7ad0e77f6aefac
Armaan Katyal <armaankatyal3@gmail.com>
2024-05-30 21:54:12 -0700

    added RequestHandler to manually handle incoming requests

--- server/main.go
+++ server/main.go
@@ -9,28 +9,60 @@ import (
 
 type ServiceRegistry struct{}
 
-func NewServiceRegistry() ServiceRegistry {
-	return ServiceRegistry{}
+func (sr *ServiceRegistry) Register(name string, address string) {}
+func (sr *ServiceRegistry) Deregister(name string)               {}
+func (sr *ServiceRegistry) GetAddress(name string) string        { return "" }
+
+func NewServiceRegistry() *ServiceRegistry {
+	return &ServiceRegistry{}
 }
 
 type RateLimiter struct{}
 
-func initializeRoutes(r *ServiceRegistry) *http.ServeMux {
+func initializeRoutes(r *RequestHandler) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /register_service", r.Register_service)
-	mux.HandleFunc("POST /deregister_service", r.Deregister_service)
+	mux.HandleFunc("/", r.Handle_request)
 	return mux
 }
 
-type RegisterBody struct{}
+type RegisterBody struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
 
 type RegisterResponse struct {
 	Message string `json:"message"`
 }
 
-type DeregisterBody struct{}
+type DeregisterBody struct {
+	Name string `json:"name"`
+}
 
-type DeregisterResponse struct{}
+type DeregisterResponse struct {
+	Message string `json:"message"`
+}
+
+type RequestHandler struct {
+	ServiceRegistry *ServiceRegistry
+}
+
+func NewRequestHandler(sr *ServiceRegistry) *RequestHandler {
+	return &RequestHandler{ServiceRegistry: sr}
+}
+
+func (rh *RequestHandler) Handle_request(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	method := r.Method
+
+	switch {
+	case method == "POST" && path == "/register_service":
+		rh.ServiceRegistry.Register_service(w, r)
+	case method == "POST" && path == "/deregister_service":
+		rh.ServiceRegistry.Deregister_service(w, r)
+	default:
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
+}
 
 func (sr *ServiceRegistry) Register_service(w http.ResponseWriter, r *http.Request) {
 	j, err := json.Marshal(RegisterResponse{Message: "Hello World"})
@@ -45,7 +77,8 @@ func (sr *ServiceRegistry) Deregister_service(w http.ResponseWriter, r *http.Req
 
 func main() {
 	sr := NewServiceRegistry()
-	router := initializeRoutes(&sr)
+	rh := NewRequestHandler(sr)
+	router := initializeRoutes(rh)
 	server := &http.Server{
 		Addr:         ":8080",
 		Handler:      router,
